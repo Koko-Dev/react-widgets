2d92f62f992eaf8929f8727c22817a1d3dbd4310
Koko-Dev <koko.webdev@gmail.com>
2022-04-01 16:03:30 -0400

    In Accordion.js function component Accordion, map through props.questions and return question and answer for each question with semantic ui accordion styling

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const Accordion = () => {
-	return <h1>Accordion</h1>
+const Accordion = ({ questions }) => {
+	const renderedQuestions = questions.map((question) => {
+		return (
+			<div key={question.question}>
+				<div className="title active">
+					<i className="dropdown icon"></i>
+					{question.question}
+				</div>
+				<div className="content active">
+					<p>{question.answer}</p>
+				</div>
+			</div>
+		)
+	})
+
+	return <div className="ui styled accordion">{renderedQuestions}</div>
+
 }
 
 export default Accordion;
