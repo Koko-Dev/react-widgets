eb77eaafc006f9f618a794524f2f0b23f3f01dcc
Koko-Dev <koko.webdev@gmail.com>
2022-04-01 11:13:06 -0400

    In App.js, create an array of objects which contain questions and answers and add this array as a prop in <Accordion />

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import Accordion from "./components/Accordion";
 
+const questions = [
+	{
+		question: 'Which instructors are the best JavaScript instructors?',
+		answer: 'Traversy, Max and Jonas'
+	},
+	{
+		question: 'What course subjects do I take to become a developer?',
+		answer: 'HTML, CSS, Advanced CSS, Javascript, and NodeJS'
+	},
+	{
+		question: 'Why isn\'t React, Vue or Angular on that list?',
+		answer: 'Because one should master the basics first'
+	},
+	{
+		question: 'How long does it take to become a great developer?',
+		answer: '10,000 hours'
+	}
+];
+
 export default () => {
 	return (
 		<div>
-			<Accordion />
+			<Accordion questions={questions} />
 		</div>
 	)
 }
