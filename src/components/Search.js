119ccd4782d5d48216381399228a1a8c535940e6
Koko-Dev <koko.webdev@gmail.com>
2022-04-05 14:06:41 -0400

    In Search.js, import useState from react; in Search function component, initialize term and setTerm to initialState empty string; also, set props value-{term} and onChange with an inline listener which updates term to event.target.value

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Search = () => {
+	const [term, setTerm] = useState('');
+
 	return (
 		<div>
 			<div className="ui form">
 				<div className="field">
 					<label htmlFor="">Enter Search Term</label>
-					<input type="text" className="input"/>
+					<input
+						type="text"
+						className="input"
+						value={term}
+						onChange={ (event) => setTerm(event.target.value)} />
 					{/* /.input */}
 				</div>
 				{/* /.field */}
