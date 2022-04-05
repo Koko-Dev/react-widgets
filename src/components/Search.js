e6875eac53ed788a25e1f55e0a57a1f620943c6b
Koko-Dev <koko.webdev@gmail.com>
2022-04-05 13:39:23 -0400

    In Search.js component return div, create an input form with one text input styled with sematinc-ui

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Search = () => {
-	return <div>Search</div>
+	return (
+		<div>
+			<div className="ui form">
+				<div className="field">
+					<label htmlFor="">Enter Search Term</label>
+					<input type="text" className="input"/>
+					{/* /.input */}
+				</div>
+				{/* /.field */}
+			</div>
+			{/* /.ui form */}
+		</div>
+	)
 }
 
 export default Search;
