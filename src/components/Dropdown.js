dbf23750a54ea4bb7b2d833f1307839c70c8c245
Koko-Dev <koko.webdev@gmail.com>
2022-04-12 15:51:51 -0400

    In Dropdown.js, we must address the TypeError that ensues when Dropdown component is removed from the DOM.  Import useRef from react; in useEfect document.body.eventListener, move body to a separate function (onBodyClick) and refer to that when calling eventListener;  also, put in a useEffect return cleanup function to remove event listener on onBodyClick to avoid TypeError in use case when Dropdown is removed from the DOM because if we don't remove it then ref.current will evaluate to null as there will no longer be div.ui.form
    // Dropdown.js useEffect()
    
        useEffect(() => {
            const onBodyClick = (event) => {
                if (ref.current.contains(event.target)) return;
                setOpen(false);
            }
            document.body.addEventListener('click', onBodyClick, {capture: true});
    
            return () => {
                document.body.removeEventListener('click', onBodyClick, {capture: true});
            }
        }, []);

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,24 +1,46 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
-const Dropdown = ({ options, selected, onSelectedChange }) => {
+const Dropdown = ({options, selected, onSelectedChange}) => {
 	const [open, setOpen] = useState(false);
 	const ref = useRef();
 
 	useEffect(() => {
-		document.body.addEventListener('click', (event) => {
+		const onBodyClick = (event) => {
 			/*TODO: Check to see if the element that was
 			   clicked on (event.target) is inside of
 			   ref.current (div.ui.form).  If it is, return early.
-		    Note: When we return early, this piece of code will run and dropdown will close:
-		    Code:
-		     <div className={`ui selection dropdown ${open ? 'visible active' : ''}`}
-		     onClick={() => setOpen(!open)}>...</div>
-				 */
+			 Note: When we return early, this piece of code will run
+			   and dropdown will close:
+				 Code:
+				  <div className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+				  onClick={() => setOpen(!open)}>...</div>
+			 */
 			if (ref.current.contains(event.target)) return;
 			setOpen(false);
-		},
-			{ capture: true}
-		);
+		}
+
+		document.body.addEventListener('click', onBodyClick, {capture: true});
+
+		// Todo:  UseCase: Dropdown Component is removed from the DOM.
+		//  Solution: Remove event listener in useEffect CLEANUP function
+		//  to avoid error:
+		//  'TypeError: Cannot read property 'contains' of null'
+		// Note:
+		//  We understand that if we should remove the
+		//  Dropdown Component from displaying on the screen the event
+		//  listener would still run, so we have to make sure to
+		//  remove it on cleanup. We would receive a
+		//  TypeError because ref prop points to ref in div.ui.form;
+		//  if there is no Dropdown Component then
+		//  ref.current evaluates to  null because we no longer
+		//  have an element (div.ui.form) to refer to.
+		//  therefore conditional 	if (ref.current.contains(event.target))
+		//  will break, i.e. if (null.contains(event.target)) = BAD
+		return () => {
+			// Note: useEffect cleanup function invoked when we
+			//  remove Dropdown Component from the DOM.
+			document.body.removeEventListener('click', onBodyClick, {capture: true});
+		}
 	}, []);
 
 
@@ -34,7 +56,8 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
 	})
 
 	return (
-		<div ref={ref} className="ui form">
+		<div ref={ref}
+		     className="ui form">
 			<div className="field">
 				<label className="label">Select a Color</label>
 				{/* /.label */}
