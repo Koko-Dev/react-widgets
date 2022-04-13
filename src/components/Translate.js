d5f63e7840ff4c9999ec7fadffbadbd87b46bc5e
Koko-Dev <koko.webdev@gmail.com>
2022-04-12 21:34:51 -0400

    Translate.js: In Translate component, set state language / setLanguage equal to useState initial value of options[0]; add props selected={language} and onSelectChange={setLanguage}

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -17,10 +17,15 @@ const options = [
 ];
 
 const Translate = () => {
+	const [language, setLanguage] = useState(options[0]);
 
 	return (
 		<div>
-			<Dropdown options={options}/>
+			<Dropdown
+				options={options}
+				selected={language}
+				onSelectedChange={setLanguage}
+			/>
 		</div>
 	)
 }
