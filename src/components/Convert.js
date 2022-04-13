a6cd1ab616f4bc769daf2e043a90e7a45168346e
Koko-Dev <koko.webdev@gmail.com>
2022-04-13 13:52:52 -0400

    Convert.js: Create a piece of state (translated / setTranslated) and set useState to an empty string;  this is where the translation of input text will be stored and set; in useEffect, create a helper async function and move axios call there; set it as an an awaited value to const { data }; after the call, setTranslated to the returned translated text; directly after helper async function, call same function

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -2,17 +2,31 @@ import React, {useState, useEffect} from "react";
 import axios from "axios";
 
 const Convert = ({ language, text }) => {
+	const [translated, setTranslated] = useState('');
 
 	useEffect(() => {
 		const url = 'https://translation.googleapis.com/language/translate/v2';
+
 		// Todo: Make request to Google API
-		axios.post(url, {}, {
-			params: {
-				q: text,
-				target: language.value,
-				key: 'Your Google Translation API Key'
-			}
-		})
+		// Todo: Helper async function which makes the call to API
+		//  through axios and targets the API data property specifically
+		const doTranslation = async () => {
+			const { data } = await axios.post(url, {}, {
+				params: {
+					q: text,
+					target: language.value,
+					key: 'Google Translation API Key'
+				}
+			})
+
+			setTranslated(data.data.translations[0].translatedText);
+		};
+
+		doTranslation()
+
+
+
+
 	}, [language, text])
 	return <div />
 }
