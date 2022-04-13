f362235f160edae47177101b5d69608e1f481c0f
Koko-Dev <koko.webdev@gmail.com>
2022-04-12 21:26:04 -0400

    In Translate.js, import React and useState from react library and Dropdown from ./Dropdown;  create const options, an array of objects with label: language and value: language abbreviation;  also, create Translate component which returns <Dropdown />; export Translate Component

---
+++ src/components/Translate.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import Dropdown from './Dropdown';
+
+const options = [
+	{
+		label: 'Africaans',
+		value: 'af'
+	},
+	{
+		label: 'Arabic',
+		value: 'ar'
+	},
+	{
+		label: 'Hindi',
+		value: 'hi'
+	}
+];
+
+const Translate = () => {
+
+	return (
+		<div>
+			<Dropdown />
+		</div>
+	)
+}
+
+export default Translate;
