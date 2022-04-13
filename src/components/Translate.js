c21bcd0f543762998b088431cb2ede0b7895d83e
Koko-Dev <koko.webdev@gmail.com>
2022-04-12 21:51:19 -0400

    Translate.js: In Translate component, add a piece of state (text / setText) whose useState initializes to an empty string; in return div, add <input /> with three props - type='text', value={text} and onChange= inline event listener which sets the text to e.target.value

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -18,9 +18,15 @@ const options = [
 
 const Translate = () => {
 	const [language, setLanguage] = useState(options[0]);
+	const [text, setText] = useState('');
 
 	return (
 		<div>
+			<input
+				type="text"
+				value={text}
+				onChange={(e) => setText(e.target.value)}/>
+	
 			<Dropdown
 				label="Select a Language"
 				options={options}
