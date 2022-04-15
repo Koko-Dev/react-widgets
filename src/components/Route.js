33d5c2117f3a9f4e6f864a36d8f14bc2a69a07c9
Koko-Dev <koko.webdev@gmail.com>
2022-04-15 13:52:31 -0400

    Route.js: Issue -- The four Route components called in Heading.js cannot detect url change when user clicks on one of the four widget headings; add an event listener which listens for the 'popstate' event we created and emitted from Link.js when the user clicks on one of the widget headings;  this step will not change the content of the page, only the URL at this point; remove the listener on cleanup; also, fix import error from importing useState to importing useEffect
    // Route.js
    import { useEffect } from "react";
    
    const Route = ({path, children}) => {
    
        useEffect(() => {
            const onLocationChange = () => {
                console.log('Location Change');
            }
            window.addEventListener('popstate', onLocationChange);
    
            // CLEANUP
            return () => {
                window.removeEventListener('popstate', onLocationChange);
            }
    
        }, [])
        return window.location.pathname === path ? children : null;
    }
    
    export default Route;
    
    In Link.js we created and emitted a 'popstate' event

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,9 +1,31 @@
-import { useState } from "react";
+import { useEffect } from "react";
 
 /*Note: Anytime we set up an event listener inside of a component,
-    that's usually a sign that we need to define a useState() hook.*/
+    that's usually a sign that we need to define a useEffect() hook.*/
 
 const Route = ({path, children}) => {
+
+	/* Important: In useEffect():
+	    Usually when we wire up an event handler we only want to call it,
+	     or start listening only once. Because of this, make sure you pass
+	     an empty array as useEffect's second argument.
+         -- The empty array signifies that we only run that
+         function when this component is first rendered to the screen.
+	*/
+	useEffect(() => {
+		// Note: Define onLocationChange separately so that it is easy
+		//  to remove on Cleanup.
+		const onLocationChange = () => {
+			console.log('Location Change');
+		}
+		window.addEventListener('popstate', onLocationChange);
+
+		// CLEANUP
+		return () => {
+			window.removeEventListener('popstate', onLocationChange);
+		}
+
+	}, [])
 	return window.location.pathname === path ? children : null;
 }
 
