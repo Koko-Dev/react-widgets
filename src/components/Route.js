4352c8763534e83ba0d53557de429cf504b7f6a8
Koko-Dev <koko.webdev@gmail.com>
2022-04-15 13:25:57 -0400

    Route.js: Anytime we set up an event listener inside of a component, which we must do here, that's usually a sign that we need to define a useEffect hook; import useState from 'react' as a first step before setting up the event listener which will listen to the 'popstate' event dispatched from Link.js onClick handler

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,5 +1,12 @@
+import { useState } from "react";
+
+/*Note: Anytime we set up an event listener inside of a component,
+    that's usually a sign that we need to define a useState() hook.*/
+
 const Route = ({path, children}) => {
 	return window.location.pathname === path ? children : null;
 }
+
+// goal: Set up an event
 
 export default Route;
