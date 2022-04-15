c9c202948a467d10fe21b92e86a6bd2a10e99649
Koko-Dev <koko.webdev@gmail.com>
2022-04-15 13:16:19 -0400

    Link.js: Issue - We can change the URL onClick with the window.history.pushState() method which can change the URL, although at this point, it cannot change the content because we have to create and emit a navigation event that tells all of our different Route Components in Header.js that the URL has changed, depending on which Navigation header link the user clicks; manually create and emit the 'popstate' event; on the next commit we will listen for this event in Route.js
    // Link.js
    import React from "react";
    
    const Link = ({ className, href, children }) => {
        const onClick = (event) => {
            event.preventDefault();
            window.history.pushState({}, '', href);
            const navEvent = new PopStateEvent('popstate');
            window.dispatchEvent(navEvent);
        }
        return (
            <a
                href={href}
                className={className}
                onClick={onClick}>
                {children}
            </a>
        )
    };
    
    export default Link;

--- src/components/Link.js
+++ src/components/Link.js
@@ -5,8 +5,22 @@ const Link = ({ className, href, children }) => {
 	//	Todo: Prevent a full page reload
 		event.preventDefault();
 
-		// goal: Change the URL without changing the content
+		// Goal:
+		//  Change the URL without changing the content
 		window.history.pushState({}, '', href);
+
+		/*
+		 Goal:  Detecting Navigation:
+			 Produce and emit a navigation event that tells all of our different
+			 Route Components that the URL has changed; To do so,
+			 Emit an event every time user clicks on one of the Headings.
+			 This Navigation event is going to be what tells
+			  all of our different <Route />s that the URL has just changed.
+			  Step: ... Listen for this newly dispatched 'popstate' event
+			   from Route.js Route Component by setting up a route handler there.
+		 */
+		const navEvent = new PopStateEvent('popstate');
+		window.dispatchEvent(navEvent);
 
 	}
 	return (
